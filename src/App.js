ea9d5d10e87e47b9c13fd3d3d0b1cdb45118a773
steviekong <sidharth.reji@gmail.com>
2020-08-22 11:22:45 +0530

    completed navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import {BrowserRouter, Route} from 'react-router-dom'
+import Navbar from './Components/Navbar'
 
 function App() {
   return (
    <BrowserRouter>
-      Add your code here
+      <Navbar/>
    </BrowserRouter>
   )
 }
--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 function NavBar() {
   return (
     <>
-      {/* add your links */}
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/about-us">About</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/login">Login</Link>
+      </div>
     </>
   )
 }
