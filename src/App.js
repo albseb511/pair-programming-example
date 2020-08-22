5013c62abedf98f0baa28fa40d5cf6eab94e9110
albseb511 <albertsebastian511@gmail.com>
2020-08-22 11:26:45 +0530

    Added: App.js code

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ function App() {
   return (
    <BrowserRouter>
       <Navbar/>
+      <div>
+        <Route exact path="/">Home Page</Route>
+        <Route path="/about-us">About Page</Route>
+        <Route path="/contact">Contact Page</Route>
+        <Route path="/services">Services Page</Route>
+        <Route path="/login">Login Page</Route>
+      </div>
    </BrowserRouter>
   )
 }
